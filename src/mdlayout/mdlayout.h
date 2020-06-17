11cb4fcfc6137f2cc4d658dfeefca619d3614dec
Claus Wilke <wilke@austin.utexas.edu>
2020-06-17 00:49:18 -0500

    clarify header

--- src/mdlayout/mdlayout.h
+++ src/mdlayout/mdlayout.h
@@ -5,7 +5,7 @@ extern "C" {
   extern char* md_to_html(const char*);
   extern void free_rust_cstring(char *); /* call to deallocate char* returned from rust */
 
-  extern void test_renderer(void *);
+  extern void test_renderer(void *gr_object);
     
 #ifdef __cplusplus
 }

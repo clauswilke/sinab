405323d1c2fa469ef783ddb4769ba55b23641efe
Claus Wilke <wilke@austin.utexas.edu>
2020-07-06 23:31:39 -0500

    iterate over dom test

--- src/mdlayout/src/test.rs
+++ src/mdlayout/src/test.rs
@@ -19,15 +19,34 @@ pub extern "C" fn mdl_test_renderer(rdev_ptr: *mut C_RenderDevice, text: *const 
 use crate::dom::*;
 use std::ops::Index;
 
+fn process_nodes(node_id: NodeId, document: &Document) {
+    let node = &document[node_id];
+    let mut eltname = "".to_string();
+
+    if let NodeData::Element(ElementData{ref name, ..}) = node.data {
+        eltname = name.local.to_string();
+        println!("<{}>", eltname);
+    }
+
+    println!("{:?}", node.data);
+
+    if let Some(child_id) = node.first_child {
+        for nid in document.node_and_following_siblings(child_id) {
+            process_nodes(nid, document);
+        }
+
+    }
+
+    if eltname != "" {
+        println!("</{}>", eltname);
+    }
+
+}
+
 pub fn test_dom() {
-    let input = "<p>Hello, <em>world!</em></p>";
+    let input = "<p>Hello, <em>world!</em> How is it going?</p><strong><em><span>test</span></em></strong>";
 
     let document = Document::parse_html(input.as_bytes());
 
-    for n in document.nodes() {
-        let node = document.index(n);
-        println!("{:?}", node.data);
-    }
-
-    println!("testing the dom");
+    process_nodes(document.root_element(), &document)
 }

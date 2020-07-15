2fc070bd2349b527b24395d232a018d32567addb
Claus Wilke <wilke@austin.utexas.edu>
2020-07-15 14:02:13 -0500

    expanded test

--- src/mdlayout/src/style/tests/selectors.rs
+++ src/mdlayout/src/style/tests/selectors.rs
@@ -8,6 +8,7 @@ use crate::{
 
 use cssparser::RGBA;
 
+/*
 fn process_node<'dom>(node_id: NodeId, author_styles: &'dom StyleSet, document: &'dom Document,
 ) {
     let style = style_for_element(author_styles, document, node_id, None);
@@ -26,7 +27,7 @@ fn process_node<'dom>(node_id: NodeId, author_styles: &'dom StyleSet, document: 
     }
 
     println!("color: {:?}", &style.color.color);
-    println!("background color: {:?}", &style.background.background_color);
+    //println!("background color: {:?}", &style.background.background_color);
 
 
     if let Some(child_id) = node.first_child {
@@ -36,6 +37,7 @@ fn process_node<'dom>(node_id: NodeId, author_styles: &'dom StyleSet, document: 
         }
     }
 }
+*/
 
 // validate that a given node is an element and has the expected type
 macro_rules! validate_element_type {
@@ -71,16 +73,22 @@ macro_rules! validate_color {
 
 #[test]
 fn selectors() {
-    let text_input = "<body><p>Hello world</p></body>";
+    // avoid whitespace in html to simplify testing
+    let text_input =
+r#"<body><p>par 1</p><p class="p2">par 2</p><p id="p3">par 3</p><span id="p3">span 1</span></body>"#;
     let css_input = r#"
-        p {color: red;}
-        body {color: green;}
+        p      { color: red; }
+        .p2    { color: blue; }
+        #p3    { color: white; }
+        body   { color: green; }
+        span   { color: yellow !important; }
     "#;
 
     let document = Document::parse_html(text_input.as_bytes());
     let author_styles = &document.parse_stylesheets(Some(css_input));
-    process_node(document.root_element(), author_styles, &document);
 
+    // uncomment next line for debugging
+    //process_node(document.root_element(), author_styles, &document);
 
     let node_id = document.root_element();
     validate_element_type!(node_id, "html", document);
@@ -94,12 +102,33 @@ fn selectors() {
     validate_element_type!(node_id, "body", document);
     validate_color!(node_id, RGBA::new(0, 128, 0, 255), document, author_styles);
 
+    // paragraph 1 --- red
     let node_id = document[node_id].first_child.unwrap();
     validate_element_type!(node_id, "p", document);
     validate_color!(node_id, RGBA::new(255, 0, 0, 255), document, author_styles);
-
-    let node_id = document[node_id].first_child.unwrap();
-    validate_text!(node_id, "Hello world", document);
+    let child = document[node_id].first_child.unwrap();
+    validate_text!(child, "par 1", document);
     // we're not propagating styles in this test example, so this should be default again
-    validate_color!(node_id, RGBA::new(0, 0, 0, 255), document, author_styles);
+    validate_color!(child, RGBA::new(0, 0, 0, 255), document, author_styles);
+
+    // paragraph 2 --- blue
+    let node_id = document[node_id].next_sibling.unwrap();
+    validate_element_type!(node_id, "p", document);
+    validate_color!(node_id, RGBA::new(0, 0, 255, 255), document, author_styles);
+    let child = document[node_id].first_child.unwrap();
+    validate_text!(child, "par 2", document);
+
+    // paragraph 3 --- white
+    let node_id = document[node_id].next_sibling.unwrap();
+    validate_element_type!(node_id, "p", document);
+    validate_color!(node_id, RGBA::new(255, 255, 255, 255), document, author_styles);
+    let child = document[node_id].first_child.unwrap();
+    validate_text!(child, "par 3", document);
+
+    // span 1 --- yellow (!important overrides id style)
+    let node_id = document[node_id].next_sibling.unwrap();
+    validate_element_type!(node_id, "span", document);
+    validate_color!(node_id, RGBA::new(255, 255, 0, 255), document, author_styles);
+    let child = document[node_id].first_child.unwrap();
+    validate_text!(child, "span 1", document);
 }

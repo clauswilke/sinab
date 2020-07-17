9e222c0f0c0ee24841c1abc91ed71459a0d63939
Claus Wilke <wilke@austin.utexas.edu>
2020-07-17 18:46:12 -0500

    cleanup

--- src/mdlayout/src/lib.rs
+++ src/mdlayout/src/lib.rs
@@ -1,7 +1,6 @@
 // Modules are other .rs source files
 pub mod renderer; // render text, rectangles, etc.
 pub mod c_helper; // support functions to interface with C
-pub mod style2;    // css styles and parsing
 pub mod dom;      // document object model
 pub mod markdown; // convert markdown to HTML
 pub mod layout;
